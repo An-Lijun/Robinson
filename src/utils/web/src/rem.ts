8ba8420fa39d47aaaa27bfa7f0d2abd9f13b8ad9
yutian <nj_yutian@qq.com>
2024-05-14 15:33:24 +0800

    fix

---
+++ src/utils/web/src/rem.ts
@@ -0,0 +1,20 @@
+  /**
+   * 设置rem根尺寸
+   */
+ export default function setRootFontSize(window, document){
+    function resize () {
+      let ww = window.innerWidth
+      if (ww > window.screen.width) {
+        window.requestAnimationFrame(resize)
+      } else {
+        if (ww > 750) {
+          ww = 750
+        }
+        document.documentElement.style.fontSize = (ww * 100) / 750 + 'px'
+      }
+    }
+
+    resize()
+
+    window.addEventListener('resize', resize)
+  },
